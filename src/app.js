96b6316ff716ae0e9d323c064b3e89bfa7365931
Joshua B <joshdev@9ci.com>
2022-02-23 11:05:35 -0700

    fix

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import Framework7 from 'framework7/lite/bundle';
 import Framework7Svelte from 'framework7-svelte';
 import App from './app.svelte';
 // adding this gets rid of the 'Cannot read properties of undefined (reading 'once')' error
-  import 'framework7/lite';
+import 'framework7/lite';
 
 Framework7.use(Framework7Svelte);
 
